e41317921d3871386403518757ea32e0790b7466
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-04-11 21:29:34 +0300

    get input function

--- app.js
+++ app.js
@@ -15,16 +15,21 @@ let male_names = [
   "Kofi",
   "Kwame",
 ];
-let female_names = [
-  "Akosua",
-  "Adowoa",
-  "Abenaa",
-  "Akua",
-  "Yaa",
-  "Afua",
-  "Ama",
-];
+//global variables
+let female_names = ["Akosua", "Adowoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 let gender;
 let raw_year, CC, MM, DD, YY, day_of_week;
-
 let result_data = document.getElementById("result");
+//get input function
+function getInput() {
+  raw_year = document.getElementById("year").value;
+  YY = raw_year.slice(0, 2);
+  CC = raw_year.slice(2, 4);
+  MM = parseInt(document.getElementById("month").value);
+  DD = parseInt(document.getElementById("day_of_month").value);
+
+  console.log(`${CC}`);
+  console.log(`${YY}`);
+  console.log(`${MM}`);
+  console.log(`${DD}`);
+}
