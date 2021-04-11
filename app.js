694f72cdd0304534a8152319183d9b4af5ece29c
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-04-11 21:24:30 +0300

    declaring female,male and days arrays

---
+++ app.js
@@ -0,0 +1,26 @@
+let days_of_week = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let male_names = [
+  "Kwasi",
+  "Kwadwo",
+  "Kwabena",
+  "Kwaku",
+  "Yaw",
+  "Kofi",
+  "Kwame",
+];
+let female_names = [
+  "Akosua",
+  "Adowoa",
+  "Abenaa",
+  "Akua",
+  "Yaa",
+  "Afua",
+  "Ama",
+];
